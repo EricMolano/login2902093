1ab71f59e657cac27498ee84fe1110090e195779
Your Name <you@example.com>
2024-07-18 17:29:44 -0500

    feat: Se agrega todo el resto del codigo

--- app/src/main/java/com/example/login2902093/screens/SignUpScreen.kt
+++ app/src/main/java/com/example/login2902093/screens/SignUpScreen.kt
@@ -1,4 +1,34 @@
 package com.example.login2902093.screens
 
-class SignUpScreen {
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.login2902093.R
+import com.example.login2902093.components.NormalTextComponent
+
+
+@Composable
+fun SignUpScreen() {
+
+    Surface(
+        color = Color.White,
+        modifier = Modifier.fillMaxSize()
+            .padding(28.dp)
+            .background(color = Color.White)
+    ) {
+        NormalTextComponent(value = stringResource(id = R.string.hello))
+    } // Surface
+} // SignUpScreen
+
+@Preview(showBackground = true)
+@Composable
+fun SignUpScreenPreview() {
+    SignUpScreen()
 }
