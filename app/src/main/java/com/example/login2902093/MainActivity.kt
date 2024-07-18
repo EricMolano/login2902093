ad384e8f9fc5506f039f2cf2e071da1c22a3633a
Your Name <you@example.com>
2024-07-18 14:55:48 -0500

    feat: Se llama a PostOfficeApp

--- app/src/main/java/com/example/login2902093/MainActivity.kt
+++ app/src/main/java/com/example/login2902093/MainActivity.kt
@@ -4,44 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.login2902093.ui.theme.Login29020093Theme
+import com.example.login2902093.app.PostOfficeApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Login29020093Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
+            PostOfficeApp()
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Login29020093Theme {
-        Greeting("Android")
-    }
-}
+        } // SetContent
+    }// OnCreate
+} // MainActivity
